a12e42899afc7d2f95f9f4dc2763d98529544fcc
Dathan Towns <dathantowns@gmail.com>
2025-04-30 19:38:58 -0700

    feat: initailize vite project/ add header without function

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import headerLogo from "../../images/Logo.png";
+import headerAvatar from "../../images/headerTerrenceAvatar.png";
+import "./Header.css";
+
+export default function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  const userName = "Terrence Wiggins";
+
+  function handleHeaderButtonClick() {
+    // NEEDS CODING!!!
+    console.log("open modalWithForm");
+  }
+
+  return (
+    <>
+      <div className="header">
+        <div className="header__date-logo">
+          <img src={headerLogo} alt="headerlogo" className="header__logo" />
+          {`${currentDate}, Bakersfield, CA`}
+        </div>
+
+        <div className="header__menu">
+          <button
+            className="header__button"
+            type="button"
+            onClick={handleHeaderButtonClick}
+          >
+            + Add clothes
+          </button>
+          {userName}
+          <img
+            src={headerAvatar}
+            alt="header avatar"
+            className="header__avatar"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
