51bc38bd60f6b3a68dcc0254355d597b7af01cd1
Dathan Towns <dathantowns@gmail.com>
2025-05-19 13:50:09 -0700

    feat: add AddItemModal

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "../src/components/App/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/se_project_react/">
+    <HashRouter basename="/se_project_react/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
